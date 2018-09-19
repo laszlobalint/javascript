bc4f98fb8a6933f455ed508fcc70b801200f152a
laszlobalint1987@gmail.com <laszlobalint1987@gmail.com>
2018-09-19 15:01:59 +0200

    Some TypeScript examples

---
+++ typescript/04-Generics/generics.ts
@@ -0,0 +1,9 @@
+// Generics are types which can hold/ use several types
+// We're only touching the very basics here - you can go MUCH more into detail
+
+let numberArray: Array <number>; // This array will only accept numbers
+
+// Try to initialize it with strings
+
+// numberArray = ['test']; // => Error
+numberArray = [1, 2, 3];
